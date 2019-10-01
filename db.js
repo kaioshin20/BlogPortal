126a4fd424697aab4def74483107b28dc84ab7d6
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-01 13:26:48 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { MongoClient } = require('mongodb')
-// const url = 'mongodb://localhost:27017'
-const url = 'mongodb+srv:<user>:<password>@cluster2019-vg1ij.mongodb.net/test?retryWrites=true&w=majority'
+const url = 'mongodb://127.0.0.1/RajatBlogApp';
+// const url = 'mongodb+srv:<user>:<password>@cluster2019-vg1ij.mongodb.net/test?retryWrites=true&w=majority'
 
 const connectdb = (dbname) => {
-    return MongoClient.connect(url)
+    return MongoClient.connect(url,{useNewUrlParser:true})
     .then(client => client.db(dbname))
 }
 
